3ecc19f095a907ba3848467e328ca19350ccec21
clevermas <clevermas@gmail.com>
2020-09-24 15:51:33 +0300

    Upgrade to Angular 10: dependencies improvement and update

--- src/app/core/stop-propagation/stop-propagation.directive.ts
+++ src/app/core/stop-propagation/stop-propagation.directive.ts
@@ -1,13 +1,11 @@
-import {Directive, HostListener} from "@angular/core";
+import { Directive, HostListener } from '@angular/core';
 
 @Directive({
-  selector: "[app-click-stop-propagation]"
+  selector: '[app-click-stop-propagation]'
 })
-export class StopPropagationDirective
-{
-  @HostListener("click", ["$event"])
-  public onClick(event: any): void
-  {
+export class StopPropagationDirective {
+  @HostListener('click', ['$event'])
+  public onClick(event: any): void {
     event.stopPropagation();
   }
 }
